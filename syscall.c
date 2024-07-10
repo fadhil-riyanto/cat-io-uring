7ae2f9d9bb5adb2d15f83ee706afb36c011b3cf9
fadhil riyanto <id.fadhil.riyanto@gmail.com>
2024-07-10 11:33:14 +0700

    rename file, and add io_uring_setup

--- syscall.c
+++ syscall.c
@@ -6,3 +6,14 @@
         427	common	io_uring_register	sys_io_uring_register
 */
 
+#include <unistd.h>
+#include <linux/io_uring.h>
+#include <linux/types.h>
+
+/* ref: https://stackoverflow.com/questions/7218220/c-u32-type-header */
+
+int io_uring_setup(__u32 entry, struct io_uring_params *p) 
+{
+        int fd = syscall(425, entry, p);
+        return fd;
+}
