2aea0c3b0f1001fb4a19e833a89d6742367cd7b8
fadhil riyanto <id.fadhil.riyanto@gmail.com>
2024-07-10 21:29:52 +0700

    add struct field, just copy from linux kernel tree

--- cat_io_uring.h
+++ cat_io_uring.h
@@ -9,12 +9,34 @@
         CQE: Completion Queue Event, kernel return in any order
 */
 
+/* warning: https://i.sstatic.net/v8efq.png */
+
+/* https://github.com/torvalds/linux/blob/34afb82a3c67f869267a26f593b6f8fc6bf35905/include/uapi/linux/io_uring.h#L462C1-L473C1 */
 struct app_io_uring_sqe_ring {
-        unsigned *head;
+        unsigned int *head;
+        unsigned int *tail;
+	unsigned int *ring_mask;
+	unsigned int *ring_entries;
+	unsigned int *flags;
+	unsigned int *dropped;
+	unsigned int *array;
+	unsigned int *resv1;
+	unsigned long user_addr;
 };
 
+
+/* https://github.com/torvalds/linux/blob/34afb82a3c67f869267a26f593b6f8fc6bf35905/include/uapi/linux/io_uring.h#L481 */
 struct app_io_uring_cqe_ring {
-        unsigned *head;
+        unsigned int head;
+	unsigned int tail;
+	unsigned int ring_mask;
+	unsigned int ring_entries;
+	unsigned int overflow;
+	unsigned int cqes;
+	unsigned int flags;
+	unsigned int resv1;
+	__u64 user_addr;
+
 };
 
 struct fileinfo {
