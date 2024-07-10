f83e7c7d50f3fd280d08d72d1c1e926fa2e5b145
fadhil riyanto <id.fadhil.riyanto@gmail.com>
2024-07-10 11:15:47 +0700

    define the io_uring_params and other struct

--- cat_io_uring.c
+++ cat_io_uring.c
@@ -1,35 +1,60 @@
+#include <linux/io_uring.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 #include "cat_io_uring.h"
 #include "io_uring_syscall.c"
 
+
+
 static int init_io_uring(submitter_t *submitter)
 {
+        /* copy the reference from malloc() */
+        struct app_io_uring_sqe_ring sqe_ring = submitter->app_io_uring_sqe_ring;
+        struct app_io_uring_cqe_ring cqu_ring = submitter->app_io_uring_cqe_ring;
+
+        /*
+                reference:
+                struct io_uring_params {
+                        __u32 sq_entries;
+                        __u32 cq_entries;
+                        __u32 flags;
+                        __u32 sq_thread_cpu;
+                        __u32 sq_thread_idle;
+                        __u32 resv[5];
+                        struct io_sqring_offsets sq_off;
+                        struct io_cqring_offsets cq_off;
+                };
+        */
+        struct io_uring_params io_params;
+        void *sqe_ptr, *cqe_ptr;
+
+        
+
 
 }
 
 static int __main(char *filename) 
 {
-    // define struct for io_uring base operation
-    submitter_t *submit = (submitter_t *)malloc(sizeof(struct submitter));
-    if (!submit) {
-        perror("alloc submit struct\n");
-        return -1;
-    }
+        // define struct for io_uring base operation
+        submitter_t *submit = (submitter_t *)malloc(sizeof(struct submitter));
+        if (!submit) {
+                perror("alloc submit struct\n");
+                return -1;
+        }
 
-    memset(submit, 0, sizeof(struct submitter));
-    
+        memset(submit, 0, sizeof(struct submitter));
 
-    return 0;
+
+        return 0;
 }
 
 int main(int argc, char **argv) 
 {
-    if (argc < 2) {
-        fprintf(stderr, "ups, usage: <filename>\n");
-        return 1;
-    }
+        if (argc < 2) {
+                fprintf(stderr, "ups, usage: <filename>\n");
+                return 1;
+        }
 
-    return __main(argv[1]);
+        return __main(argv[1]);
 }
