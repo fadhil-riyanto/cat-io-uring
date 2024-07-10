b50e2a80c41c569264a99ca77d5340b6365e5b7b
fadhil riyanto <id.fadhil.riyanto@gmail.com>
2024-07-10 10:40:21 +0700

    re-order struct, add new field for native iouring sqe ring

--- cat_io_uring.h
+++ cat_io_uring.h
@@ -1,10 +1,8 @@
 #ifndef CAT_IO_URING_H
 #define CAT_IO_URING_H
 
-#include <sys/types.h>
-typedef struct submitter {
-    int ring_fd;
-} submitter_t;
+#include <sys/uio.h>
+#include <linux/io_uring.h>
 
 /*
         SQE : Submission Queue Event, the application submit SQE
@@ -23,5 +21,13 @@ struct fileinfo {
     off_t file_sz;  /* obtained from st.file_sz */
     struct iovec iovecs[]; /* readv and writev syscall*/
 };
+
+typedef struct submitter {
+    int ring_fd;
+    struct app_io_uring_sqe_ring app_io_uring_sqe_ring;
+    struct io_uring_sqe *sqe_ring;
+    struct app_io_uring_cqe_ring app_io_uring_cqe_ring;
+
+} submitter_t;
 
 #endif
